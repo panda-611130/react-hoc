45207334370e780aa81124ddab530ec7229fe8b9
panda-611130 <13971541680@sina.cn>
2021-02-27 12:54:07 +0800

    feat: 框架搭建

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./app.less";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  hashHistory,
+} from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="root-app">
+      <Router history={hashHistory}>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/users">Users</Link>
+              </li>
+            </ul>
+          </nav>
+
+          {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/about" render={() => <div>about</div>}></Route>
+            <Route path="/users" render={() => <div>users</div>}></Route>
+            <Route path="/" render={() => <div>Home</div>}></Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
